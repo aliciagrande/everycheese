f315aed6271e393a0b776b631a4e075c369df31a
Alicia Grande <aliciagrande@Alicias-MacBook-Pro.local>
2020-09-21 09:56:40 -0300

    Add cheeses app

---
+++ everycheese/cheeses/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from autoslug import AutoSlugField
+from model_utils.models import TimeStampedModel
+
+class Cheese(TimeStampedModel):
+    name = models.CharField("Name of Cheese", max_length=255)
+    
+    slug = AutoSlugField("Cheese Addres", 
+        unique=True, always_update=False, populate_from="name")
+    
+    description = models.TextField("Description", blank=True)
+
+    class Firmness(models.TextChoices):
+        UNSPECIFIED = "unspecified", "Unspecified"
+        SOFT = "soft", "Soft"
+        SEMI_SOFT = "semi-soft", "Semi-Soft"
+        SEMI_HARD = "semi-hard", "Semi-Hard"
+        HARD = "hard", "Hard"
+    
+    firmness = models.CharField("Firmness", max_length=20, choices=Firmness.choices, default=Firmness.UNSPECIFIED)
+
+    def __str__(self):
+        return self.name
