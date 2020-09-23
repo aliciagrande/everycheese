6101858aeeb204e98e73dad5c7b7784f598b0c87
Alicia Grande <aliciagrande@Alicias-MacBook-Pro.local>
2020-09-23 11:47:43 -0300

    Add test for get_absolute_url() method

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -16,3 +16,8 @@ def test___str__():
     cheese = CheeseFactory()
     assert cheese.__str__() == cheese.name
     assert str(cheese) == cheese.name
+
+def test_get_absolute_url():
+    cheese = CheeseFactory()
+    url = cheese.get_absolute_url()
+    assert url == f'/cheeses/{cheese.slug}/'
